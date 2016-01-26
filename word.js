3adbf05046e689078147556ba716c3c2e29d32e2
chriscampbell32 <chriscampbell32@yahoo.com>
2016-01-26 13:47:00 -0500

    word function donzo

--- word.js
+++ word.js
@@ -1,14 +1,23 @@
 // import letter.js
+var Letter = require(".letter.js");
+
 
 //contructr fumction Called Word
+function Word(wrd){
+    this.word = wrd; //arg wrd
+    this.lets = []; //property called lets = [];
+    this.found = false; //property called found = false
+    //function inside called getlets run loop over each letter in word property
+    this.getlets = function() {
+        for (var i = 0; i < this.word.length; i++){
+            this.lets.push(new Letter(this.word[i]));
+        }
+    }; 
+}
 
-//arg wrd
-//propery called word store wrd
 
 
 
-//property called lets = [];
 
-//property called found = false
 
-//function inside called getlets run loop over each letter in word property
+
