7bb9c3c79b6be0ffc4979b33a9e3fdbacbc40f4f
chriscampbell32 <chriscampbell32@yahoo.com>
2016-01-26 18:30:10 -0500

    only works when i comment our getLets() in line 19.. dot understand why it says this.currentWrd.getlets is not a function

--- main.js
+++ main.js
@@ -9,12 +9,13 @@ var game = {
     wordBank: ["morris", "essex", "union", "somerset", "hunterdon", "middlesex", "bergen", "ocean", "monmouth"],
     guessesRemaining: 10,
     currentWrd: null,
-    startGame: function(wrd){
+
 //var randomNumber
 //this.currentWrd.getLets();
-startGame: function(wrd) {
+    startGame: function(wrd) {
     var randomNumber = Math.floor(Math.random() * this.wordBank.length);
     this.currentWrd = new Word(this.wordBank[randomNumber]);
+
     this.currentWrd.getLets();
     this.keepPromptingUser();
   },
@@ -22,7 +23,7 @@ startGame: function(wrd) {
         var self = this;
         prompt.get(["guessLetter"], function(err, result){
             console.log("The Letter or space you guessed is " + result.guessLetter);
-            var = findHowManyOfUserGuess = self.currentWrd.checkIfLetterFound(result.guessLetter);
+            var  findHowManyOfUserGuess = self.currentWrd.checkIfLetterFound(result.guessLetter);
             if(findHowManyOfUserGuess === 0){
                 console.log("You guess Wrong!");
                 self.guessesRemaining -= 1;
@@ -35,7 +36,19 @@ startGame: function(wrd) {
             }
             console.log("Guesses remaining " + self.guessesRemaining);
             console.log(self.currentWrd.wordRender());
-        }
-    }
 
+               
+            
+            if(self.guessesRemaining > 0 && self.currentWrd.found === false){
+                self.keepPromptingUser();
+            } else if(guessesRemaining === 0){
+                console.log("Game over bro");
+                console.log("the word was " + self.currentWrd.word);
+            } else{
+                console.log(self.currentWrd.wordRender());
+            }
+
+});
 }
+};
+game.startGame();
