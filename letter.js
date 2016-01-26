29116a2f11224c79588d91210d3db5761c95122d
chriscampbell32 <chriscampbell32@yahoo.com>
2016-01-26 11:25:21 -0500

    create constructr function

--- letter.js
+++ letter.js
@@ -1,15 +1,13 @@
 //function called letter
 function Letter(let){
     //property called charac to store arg. let
-
-    this.charac
+     this.charac = let;
+     //appear set to false
+     this.appear = false;
 }
 
 //let argument
 
 
-//property called charac to store arg. let
-
-//appear set to false
 
 //function inside letter called letterRender check values of charac if false return  a syring"_", else create charc
