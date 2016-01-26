442d2de34efd8d61f5d55887cbe80e2306a524d6
chriscampbell32 <chriscampbell32@yahoo.com>
2016-01-26 16:03:10 -0500

    create game variable

--- main.js
+++ main.js
@@ -5,4 +5,16 @@ var prompt = require("prompt");
 
 prompt.start();
 
-var game = {}
+var game = {
+    wordBank: ["morris", "essex", "union", "somerset", "hunterdon", "middlesex", "bergen", "ocean", "monmouth"],
+    guessesRemaining: 10,
+    currentWrd: null,
+    startGame: function(wrd){
+//var randomNumber
+//this.currentWrd.getLets();
+this.keepPromptingUser();
+
+
+    },
+    
+}
