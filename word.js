96796a50421be7c50a415d8d437a486cf38cfbc8
chriscampbell32 <chriscampbell32@yahoo.com>
2016-01-26 11:42:26 -0500

    started psuedo code on word.js

---
+++ word.js
@@ -0,0 +1,14 @@
+// import letter.js
+
+//contructr fumction Called Word
+
+//arg wrd
+//propery called word store wrd
+
+
+
+//property called lets = [];
+
+//property called found = false
+
+//function inside called getlets run loop over each letter in word property
