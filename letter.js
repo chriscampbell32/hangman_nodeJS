d7fb3a2d8ec8bf4500403799b8ea8e9a9ee1f1b9
chriscampbell32 <chriscampbell32@yahoo.com>
2016-01-26 11:21:58 -0500

    started letter.js

---
+++ letter.js
@@ -0,0 +1,15 @@
+//function called letter
+function Letter(let){
+    //property called charac to store arg. let
+
+    this.charac
+}
+
+//let argument
+
+
+//property called charac to store arg. let
+
+//appear set to false
+
+//function inside letter called letterRender check values of charac if false return  a syring"_", else create charc
