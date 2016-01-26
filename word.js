be51518be0facc9ae5cdd1704eeeb6aa569eae86
chriscampbell32 <chriscampbell32@yahoo.com>
2016-01-26 14:25:35 -0500

    finished word.js

--- word.js
+++ word.js
@@ -12,8 +12,33 @@ function Word(wrd){
         for (var i = 0; i < this.word.length; i++){
             this.lets.push(new Letter(this.word[i]));
         }
-    }; 
+    };
+
+    this.checkIfLetterFound = function(guessLetter){
+    var whatToReturn = 0;
+    for(var i = 0; i < this.lets.length; i++){
+      if(this.lets[i].charac === guessLetter){
+        this.lets[i].appear = true;
+        whatToReturn += 1;
+      }
+    } 
+    return whatToReturn;
+  }
+  this.didWeFindTheWord = function(){
+    if (this.lets.every(curLet) === true){
+      return this.found;
+    } 
+  } 
+  this.wordRender = function(){
+    var str = "";
+    for (var i = 0; i < this.lets.length; i++){
+        str += this.lets[i].letterRender();
+    }
+    return str;
+  };
+
 }
+module.exports = Word;
 
 
 
